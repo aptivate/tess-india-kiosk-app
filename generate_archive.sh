96312543e9b81d3a5a1ac67f10bb56bda625ecdd
Daniel Levy <daniell@aptivate.org>
2015-04-21 15:09:04 +0100

    Make the SiteArchive/generated_content directory if it doesn't exist

--- generate_archive.sh
+++ generate_archive.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+if [ -d "SiteArchive/generated_content" ]; then
+   mkdir SiteArchive/generated_content
+fi
 
 find SiteArchive/urls -type f -exec ls -1 {} \; 2> /dev/null > SiteArchive/generated_content/tess_urls_file
 cat SiteArchive/generated_content/tess_urls_file | xargs -i cat {} > SiteArchive/generated_content/tess_oer_urls.txt
