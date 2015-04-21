ec2b4f2767356bfb25fafd0c00535e82d881a1df
Daniel Levy <daniell@aptivate.org>
2015-04-21 14:37:21 +0100

    Fix paths in generate archive script

--- generate_archive.sh
+++ generate_archive.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-find ./urls -type f -exec ls -1 {} \; 2> /dev/null > generated_content/tess_urls_file
-cat generated_content/tess_urls_file | xargs -i cat {} > generated_content/tess_oer_urls.txt
-wget -N --no-remove-listing --adjust-extension --page-requisites --convert-links -np --user-agent=Mozilla -i tess_oer_urls.txt -P generated_content
+find SiteArchive/urls -type f -exec ls -1 {} \; 2> /dev/null > SiteArchive/generated_content/tess_urls_file
+cat SiteArchive/generated_content/tess_urls_file | xargs -i cat {} > SiteArchive/generated_content/tess_oer_urls.txt
+wget -N --no-remove-listing --adjust-extension --page-requisites --convert-links -np --user-agent=Mozilla -i SiteArchive/generated_content/tess_oer_urls.txt -P SiteArchive/generated_content
