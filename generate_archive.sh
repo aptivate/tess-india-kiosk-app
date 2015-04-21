b97ab273a336386cee410c1f4422b6b8deb30cc5
Daniel Levy <daniell@aptivate.org>
2015-04-21 15:37:37 +0100

    Remove debug statement

--- generate_archive.sh
+++ generate_archive.sh
@@ -2,7 +2,7 @@
 if [ ! -d "SiteArchive/generated_content" ]; then
    mkdir SiteArchive/generated_content
 fi
-exit
+
 find SiteArchive/urls -type f -exec ls -1 {} \; 2> /dev/null > SiteArchive/generated_content/tess_urls_file
 cat SiteArchive/generated_content/tess_urls_file | xargs -i cat {} > SiteArchive/generated_content/tess_oer_urls.txt
 wget -N --no-remove-listing --adjust-extension --page-requisites --convert-links -np --user-agent=Mozilla -i SiteArchive/generated_content/tess_oer_urls.txt -P SiteArchive/generated_content
