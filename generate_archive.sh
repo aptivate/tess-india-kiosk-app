5037236532a55983d91964fd031336a766779361
Hamish Downer <hamish@aptivate.org>
2015-04-28 15:55:55 +0100

    improvements to generate_archive.sh
    * script can be run from another directory
    * use named env vars for directory paths
    * add a "small" option to just download part of the site
    * use long option names in wget, and split across lines, for readability

--- generate_archive.sh
+++ generate_archive.sh
@@ -1,8 +1,33 @@
-#!/bin/sh
-if [ ! -d "SiteArchive/generated_content" ]; then
-   mkdir SiteArchive/generated_content
+#!/bin/bash
+
+# get the directory the script is in
+# from http://stackoverflow.com/a/246128/3189
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+GENERATED_DIR=$SCRIPT_DIR/SiteArchive/generated_content
+TESS_URLS_FILE=$GENERATED_DIR/tess_urls_file
+TESS_OER_URLS=$GENERATED_DIR/tess_oer_urls.txt
+
+ARCHIVE_URLS=$SCRIPT_DIR/SiteArchive/urls
+
+if [ ! -d "$GENERATED_DIR" ]; then
+   mkdir $GENERATED_DIR
 fi
 
-find SiteArchive/urls -type f -exec ls -1 {} \; 2> /dev/null > SiteArchive/generated_content/tess_urls_file
-cat SiteArchive/generated_content/tess_urls_file | xargs -i cat {} > SiteArchive/generated_content/tess_oer_urls.txt
-wget -N --no-remove-listing --adjust-extension --page-requisites --convert-links -np --user-agent=Mozilla -i SiteArchive/generated_content/tess_oer_urls.txt -P SiteArchive/generated_content
+# using the argument "small" means that only one web page will be downloaded (plus it's videos, CSS etc)
+if [ $1 = 'small' ]; then
+    cp $ARCHIVE_URLS/tess_india_video_resources/planning_lessons_urls $TESS_OER_URLS
+else
+    find $ARCHIVE_URLS -type f -exec ls -1 {} \; 2> /dev/null > $TESS_URLS_FILE
+    cat $TESS_URLS_FILE | xargs -i cat {} > $TESS_OER_URLS
+fi
+
+wget --timestamping \
+    --no-remove-listing \
+    --adjust-extension \
+    --page-requisites \
+    --convert-links \
+    --no-parent \
+    --user-agent=Mozilla \
+    --input-file=$TESS_OER_URLS \
+    --directory-prefix=$GENERATED_DIR
