4e0585ee71b4e0a16a2b5ed5396f34b7d9108b53
Marko Samastur <markos@gaivo.net>
2015-04-29 15:26:47 +0200

    New logos and mobile fixes

--- archive_optimize/archive_optimize.py
+++ archive_optimize/archive_optimize.py
@@ -3,6 +3,7 @@
 import os
 import sys
 from distutils import dir_util
+from shutil import copyfile
 
 from bs4 import BeautifulSoup
 import magic
@@ -25,6 +26,25 @@ def add_mobile_css(soup):
 def remove_inline_styles(soup):
     for inlined in soup.select('[style]'):
         del inlined['style']
+
+
+def remove_unwanted_blocks(soup):
+    selectors = (
+        '#page-openlearnworks-header',
+        '#page-openlearnworks-rhs',
+        '#page-header h1.header-title',
+        '#enrolbutton ',
+        '#region-pre',
+        '.left.side',
+        '.right.side',
+        '.page-footer-links',
+        '#page-footer-copyright',
+        '#footer1'
+    )
+    for selector in selectors:
+        matches = soup.select(selector)
+        for match in matches:
+            match.extract()
 
 
 def find_related_video_link(obj):
@@ -57,12 +77,35 @@ def replace_youtube_videos(soup):
             obj.extract()
 
 
-def process_page(content):
+def add_dfid_logo(soup, filename):
+    prefix = "../.." if 'openlearnworks/course/' in filename else "../../.."
+    dfid_logo = os.path.join(prefix, 'tessindia/UK-AID-small.jpg')
+    style = '''
+#footer3 {
+    background: url("%s") no-repeat scroll 0px 0px transparent;
+    width: 113px;
+    height: 125px;
+}
+    ''' % dfid_logo
+
+    dfid = soup.new_tag("style")
+    dfid.append(style)
+    soup.head.append(dfid)
+
+    footer3 = soup.new_tag("div", id="footer3")
+    footer_root = soup.find("div", id="page-footer-image")
+    if footer_root:
+        footer_root.append(footer3)
+
+
+def process_page(content, filename):
     soup = BeautifulSoup(content)
 
     add_mobile_css(soup)
     remove_inline_styles(soup)
+    remove_unwanted_blocks(soup)
     replace_youtube_videos(soup)
+    add_dfid_logo(soup, filename)
 
     return soup.prettify()
 
@@ -71,10 +114,19 @@ def process_file(filename):
     content = ''
     with open(filename, 'r') as f:
         orig_content = f.read()
-        content = process_page(orig_content)
+        content = process_page(orig_content, filename)
 
     with open(filename, 'w') as f:
         f.write(content.encode('utf8'))
+
+
+def copy_new_logo(copy_dir):
+    new_logo_path = os.path.join(MEDIA_DIR,
+                                 'tessindia/TESS-India-Banner.jpg')
+    old_logo_path = os.path.join(
+        copy_dir,
+        'openlearnworks/pluginfile.php/134332/theme_openlearnworks/image/Labspace-TESS-India-Banner2.jpg')
+    copyfile(new_logo_path, old_logo_path)
 
 
 def is_webpage(filename):
@@ -93,7 +145,9 @@ def main(orig_dir, copy_dir):
         print 'Destination directory already exists. Exiting!'
         sys.exit(1)
     dir_util.copy_tree(MEDIA_DIR, copy_dir)
+    print 'Making a copy of content...'
     dir_util.copy_tree(orig_dir, copy_dir)
+    copy_new_logo(copy_dir)
     for root, dirs, files in os.walk(copy_dir):
         if len(files):
             for name in files:
@@ -105,6 +159,6 @@ def main(orig_dir, copy_dir):
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print 'Not enough parameters.\n\nUsage: tess_optimize <content_dir> <output_dir>'
+        print 'Not enough parameters.\n\nUsage: archive_optimize.py <content_dir> <output_dir>'
         sys.exit(1)
     main(sys.argv[1], sys.argv[2])
