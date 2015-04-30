7a60d4cacd8bcff80cb32aacb11cd60c4a8dada9
Hamish Downer <hamish@aptivate.org>
2015-04-30 16:40:22 +0100

    reduce extra copies of stuff

--- generate_site.sh
+++ generate_site.sh
@@ -8,19 +8,21 @@ MARKDOWN_DIR=$SCRIPT_DIR/WebSource/markdown
 HTML_DIR=$SCRIPT_DIR/WebSource/html
 OUT_DIR=$SCRIPT_DIR/WebContent
 
-# convert markdown files
-for mkdfile in $MARKDOWN_DIR/*.mkd ; do
-    outfile=$HTML_DIR/$(basename ${mkdfile%.mkd}).html
-    cat $MARKDOWN_DIR/head.html > $outfile
-    markdown $mkdfile >> $outfile
-    cat $MARKDOWN_DIR/foot.html >> $outfile
-done
-
 # remove old output
 rm -rf $OUT_DIR
 mkdir $OUT_DIR
 # the empty file is for git
 touch $OUT_DIR/empty
 
-# put new content in there
+# copy HTML, images, CSS etc
 cp -r $HTML_DIR/* $OUT_DIR
+
+# convert markdown files
+for mkdfile in $MARKDOWN_DIR/*.mkd ; do
+    outfile=$OUT_DIR/$(basename ${mkdfile%.mkd}).html
+    cat $MARKDOWN_DIR/head.html > $outfile
+    markdown $mkdfile >> $outfile
+    cat $MARKDOWN_DIR/foot.html >> $outfile
+done
+
+
