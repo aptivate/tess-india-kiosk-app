a307b1ba76149b515f4dbf81f806de437bc09e59
Hamish Downer <hamish@aptivate.org>
2015-04-30 18:41:18 +0100

    Complete the process of pre-processing and copying
    according to the new scheme, and document in the README.
    
    We fix the position of a few directories in this, but it makes the
    process easier to repeat

--- tasks.py
+++ tasks.py
@@ -1,35 +1,30 @@
 import os
-from fabric.api import sudo, put, run
+from fabric.api import sudo, run
+from fabric.contrib.project import rsync_project
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def copy_content_to_pi():
+    run('rm -rf /tmp/tess')
+    run('mkdir /tmp/tess')
+
+    webcontent = os.path.join(BASE_DIR, 'WebContent/')
+    # --copy-links means copy the content from the symlink target
+    # which means we'll copy the optimized archive, which is what
+    # we want
+    rsync_project(
+        '/tmp/tess',
+        local_dir=webcontent,
+        delete=True,
+        extra_opts="--copy-links",
+    )
 
 
 def delete_old_content_on_pi():
     sudo('rm -rf /var/www/*')
 
 
-def copy_content_to_pi(srcdir):
-    src = srcdir
-    run('rm -rf /tmp/tess')
-    run('mkdir /tmp/tess')
-    if src[-1] != '/':
-        src += '/'
-    src += '*'
-    put(src, '/tmp/tess')
-
-    webcontent = os.path.join(BASE_DIR, 'WebContent/*')
-    put(webcontent, '/tmp/tess')  # Copy index.html with resources
-
-
 def move_content_to_www():
     sudo('mv /tmp/tess/* /var/www/')
-
-
-def link_first_page():
-    echo_cmd = 'Redirect 302 /tess.html /openlearnworks/course/view.php%3fid=1911.html'
-    sudo('rm -f /var/www/.htaccess')
-    sudo('echo "%s" > /tmp/.htaccess' % echo_cmd)
-    sudo('mv /tmp/.htaccess /var/www/.htaccess')
-    sudo('chown root /var/www/.htaccess')
-    sudo('service apache2 restart')
