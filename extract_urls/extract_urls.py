fe7c4d714ffdd454efd80b0217828bc9e9e85e1d
Daniel Levy <daniell@aptivate.org>
2015-05-05 17:02:51 +0100

    Start making changes to save urls instead of printing them

--- extract_urls/extract_urls.py
+++ extract_urls/extract_urls.py
@@ -1,14 +1,48 @@
 #!/usr/bin/env python
 
 from bs4 import BeautifulSoup
+import os
 import requests
 import re
+import shutil
 
 
 start_page = 'http://www.open.edu/openlearnworks/course/view.php?id=1911'
 content_identifier = 'oucontent'
 subpage_identifier = 'subpage'
 link_list_item_regex = '(subpage|oucontent)'
+current_directory = os.path.dirname(__file__)
+generated_urls_parent_directory = os.path.join(
+    current_directory,
+    '..',
+    'SiteArchive',
+    'generated_content'
+)
+generated_urls_main_directory = os.path.join(
+    generated_urls_parent_directory,
+    'urls'
+)
+
+
+def clean_file_name(file_name):
+    file_name = re.sub('[^A-Za-z0-9 ]+', '', file_name)
+    file_name = file_name.replace(' ', '_')
+    return file_name + '.links'
+
+
+def save_urls(directory, file_name, urls):
+    if not isinstance(urls, list):
+        print "The urls to be saved should be passed in as a list."
+        exit()
+
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+
+    file_path = os.path.join(directory, clean_file_name(file_name))
+
+    with open(file_path, 'a') as file:
+        urls_to_save = "\n".join(urls)
+        file.write(urls_to_save)
 
 
 def convert_page_to_soup(url):
@@ -48,15 +82,29 @@ def top_level_page_parser(url):
 
     page = the_soup.findAll('li', class_=re.compile(link_list_item_regex))
 
+    links = []
+
     for list_item in page:
         link = list_item.find('a', href=True)
         if link:
             mid_level_page_parser(link.attrs['href'])
-            print link.attrs['href']
+            links.append(link.attrs['href'])
+
+    save_urls(generated_urls_main_directory, 'main', links)
+
+
+def setup_directories():
+    if os.path.exists(generated_urls_main_directory):
+        shutil.rmtree(generated_urls_main_directory)
+
+    os.mkdir(generated_urls_main_directory)
 
 
 def main():
+    setup_directories()
+
     top_level_page_parser(start_page)
+
 
 if __name__ == '__main__':
     main()
