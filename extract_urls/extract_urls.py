80c344b8768f0c5ab064cb7b27d15a8cb241f47b
Daniel Levy <daniell@aptivate.org>
2015-05-05 16:02:59 +0100

    Remove link count

--- extract_urls/extract_urls.py
+++ extract_urls/extract_urls.py
@@ -17,53 +17,44 @@ def convert_page_to_soup(url):
 
 def print_menu_links(soup):
     menu_section = soup.find('li', class_='type_activity depth_4 item_with_icon current_branch')
-    link_count = 0
     if menu_section:
         for link in menu_section.findAll('a', href=True):
-            link_count += 1
             print link.attrs['href']
-    return link_count
 
 
 def mid_level_page_parser(url):
     soup = convert_page_to_soup(url)
     page = soup.findAll('li', class_=re.compile('(subpage|oucontent)'))
-    link_count = 0
+
     for list_item in page:
         link = list_item.find('a', href=True)
+
         if link:
-            link_count += 1
             if subpage_identifier in link.attrs['href']:
                 thin_soup = convert_page_to_soup(link.attrs['href'])
-                link_count += print_menu_links(thin_soup)
+                print_menu_links(thin_soup)
             elif 'subpage' in link.attrs['href']:
-                    link_count += mid_level_page_parser(link.attrs['href'])
+                    mid_level_page_parser(link.attrs['href'])
                     print link.attrs['href']
             else:
                 print link.attrs['href']
-    return link_count
 
 
 def top_level_page_parser(url):
     web_page = requests.get(url)
     the_soup = BeautifulSoup(web_page.text)
-    link_count = 0
 
     page = the_soup.findAll('li', class_=re.compile('(subpage|oucontent)'))
 
     for list_item in page:
         link = list_item.find('a', href=True)
         if link:
-            link_count += 1
-            link_count += mid_level_page_parser(link.attrs['href'])
+            mid_level_page_parser(link.attrs['href'])
             print link.attrs['href']
-
-    return link_count
 
 
 def main():
-    link_count = top_level_page_parser(start_page)
-    print link_count
+    top_level_page_parser(start_page)
 
 if __name__ == '__main__':
     main()
