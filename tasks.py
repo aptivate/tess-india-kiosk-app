2bd63588b12f897ff5a1cb1084b32f0440d28518
Hamish Downer <hamish@aptivate.org>
2015-05-05 15:30:13 +0100

    fab can now deploy to mirror
    and could be updated to deploy the apache config to the pi

--- tasks.py
+++ tasks.py
@@ -1,25 +1,30 @@
 import os
-from fabric.api import sudo, run
+from fabric.api import cd, env, run, settings, sudo
 from fabric.contrib.project import rsync_project
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+TMP_DIR = '/tmp/tess'
 
 
-def copy_content_to_pi():
-    run('rm -rf /tmp/tess')
-    run('mkdir /tmp/tess')
-
-    webcontent = os.path.join(BASE_DIR, 'WebContent/')
+def _rsync_web_content(local_dir, remote_dir):
     # --copy-links means copy the content from the symlink target
     # which means we'll copy the optimized archive, which is what
     # we want
     rsync_project(
-        '/tmp/tess',
-        local_dir=webcontent,
+        remote_dir,
+        local_dir=local_dir,
         delete=True,
         extra_opts="--copy-links",
     )
+
+
+def copy_content_to_pi():
+    run('rm -rf {}'.format(TMP_DIR))
+    run('mkdir {}'.format(TMP_DIR))
+
+    webcontent = os.path.join(BASE_DIR, 'WebContent/')
+    _rsync_web_content(TMP_DIR, webcontent)
 
 
 def delete_old_content_on_pi():
@@ -27,4 +32,30 @@ def delete_old_content_on_pi():
 
 
 def move_content_to_www():
-    sudo('mv /tmp/tess/* /var/www/')
+    sudo('mv {}/* /var/www/'.format(TMP_DIR))
+
+
+def git_clone_pull(code_dir, repo):
+    with settings(warn_only=True):
+        if run("test -d %s" % code_dir).failed:
+            sudo("git clone %s %s" % (repo, code_dir))
+    with cd(code_dir):
+        sudo("git pull")
+
+
+def link_apache_conf(vcs_path):
+    apache_conf = 'tessindia.conf'
+    vcs_apache_path = os.path.join(vcs_path, 'apache', env.apache)
+    if env.linux == 'redhat':
+        apache_conf_path = '/etc/httpd/conf.d'
+        service = 'httpd'
+    elif env.linux == 'debian':
+        apache_conf_path = '/etc/apache2/sites-enabled'
+        service = 'apache2'
+    else:
+        raise Exception('You need to set env.linux')
+
+    with cd(apache_conf_path):
+        sudo('rm -f {}'.format(apache_conf))
+        sudo('ln -s {} {}'.format(vcs_apache_path, apache_conf))
+    sudo('service {} reload'.format(service))
