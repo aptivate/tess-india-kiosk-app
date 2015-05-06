21cc1dcbaa5199d42d70dab9dd252c964b509dd6
Daniel Levy <daniell@aptivate.org>
2015-05-06 12:48:25 +0100

    Urls are now saved into files that give rough reflection of site structure

--- extract_urls/extract_urls.py
+++ extract_urls/extract_urls.py
@@ -27,7 +27,19 @@ generated_urls_main_directory = os.path.join(
 def clean_file_name(file_name):
     file_name = re.sub('[^A-Za-z0-9 ]+', '', file_name)
     file_name = file_name.replace(' ', '_')
-    return file_name + '.links'
+    return file_name
+
+
+def create_directories(directory):
+    parent_directories = directory.split('/')
+    if len(parent_directories) > 1:
+        immediate_parent = os.path.join(*parent_directories[:-1])
+        if not immediate_parent[0] == '/':
+            immediate_parent = '/' + immediate_parent 
+        
+        if not os.path.exists(immediate_parent):
+            create_directories(immediate_parent)
+    os.mkdir(directory)
 
 
 def save_urls(directory, file_name, urls):
@@ -36,9 +48,9 @@ def save_urls(directory, file_name, urls):
         exit()
 
     if not os.path.exists(directory):
-        os.mkdir(directory)
+        create_directories(directory)
 
-    file_path = os.path.join(directory, clean_file_name(file_name))
+    file_path = os.path.join(directory, clean_file_name(file_name)) + '.links'
 
     with open(file_path, 'a') as file:
         urls_to_save = "\n".join(urls)
@@ -51,29 +63,41 @@ def convert_page_to_soup(url):
     return soup
 
 
-def print_menu_links(soup):
+def print_menu_links(soup, directory=generated_urls_main_directory):
     menu_section = soup.find('li', class_='type_activity depth_4 item_with_icon current_branch')
+    title = soup.find('title')
+    file_name = clean_file_name(title.text)
+    links = []
+    
     if menu_section:
         for link in menu_section.findAll('a', href=True):
-            print link.attrs['href']
+            links.append(link.attrs['href'])
+    
+    save_urls(directory, file_name, links)    
 
 
-def mid_level_page_parser(url):
+def mid_level_page_parser(url, directory=generated_urls_main_directory):
     soup = convert_page_to_soup(url)
     page = soup.findAll('li', class_=re.compile(link_list_item_regex))
-
+    
+    title = soup.find('title')
+    directory = os.path.join(directory, clean_file_name(title.text))
+    
+    links = []
+    
     for list_item in page:
         link = list_item.find('a', href=True)
 
         if link:
             if content_identifier in link.attrs['href']:
                 thin_soup = convert_page_to_soup(link.attrs['href'])
-                print_menu_links(thin_soup)
+                print_menu_links(thin_soup, directory)
             elif subpage_identifier in link.attrs['href']:
-                    mid_level_page_parser(link.attrs['href'])
-                    print link.attrs['href']
+                    mid_level_page_parser(link.attrs['href'], directory)
             else:
-                print link.attrs['href']
+                links.append(link.attrs['href'])
+    
+    save_urls(directory, 'main', links)  
 
 
 def top_level_page_parser(url):
@@ -97,7 +121,7 @@ def setup_directories():
     if os.path.exists(generated_urls_main_directory):
         shutil.rmtree(generated_urls_main_directory)
 
-    os.mkdir(generated_urls_main_directory)
+    create_directories(generated_urls_main_directory)
 
 
 def main():
